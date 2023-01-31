fbc2a15f0a7fe0dd8d1fd30b6970555ae47102d0
a3520110125@alumno.uttehuacan.edu.mx <a3520110125@alumno.uttehuacan.edu.mx>
2023-01-30 23:10:38 -0600

    second commit

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import "node_modules/jquery/dist/jquery.min.js";
+import "node_modules/@popperjs/core/dist/umd/popper.min.js";
+import "node_modules/bootstrap/dist/js/bootstrap.min.js";
 import { AppModule } from './app/app.module';
 
 
